aa93c8b63a9d6cdb3e3e02adbf7614f4b62e81c2
Your Name <you@example.c>
2018-08-13 02:26:23 -0400

    fixed bug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,9 @@
 // Data that  holds all of the possible friends.
 var friends = [
 	{
-	  name: "Matt Bomer",
-	  photo: "https://m.media-amazon.com/images/M/MV5BMjA5NTE4NTE5NV5BMl5BanBnXkFtZTcwMTcyOTY5Mw@@._V1_UY209_CR13,0,140,209_AL_.jpg",
-		"scores[]": ["5", "1", "4", "4", "2", "1", "5", "2", "5", "4"]
-	},
-	{
-	  name: "Brad Pitt",
-	  photo: "https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX140_CR0,0,140,209_AL_.jpg",
-	  "scores[]": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"
-	  ]
-	},
-	{
-	  name: "Leonardo DiCaprio",
-	  photo: "https://m.media-amazon.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_UY209_CR7,0,140,209_AL_.jpg",
-	  "scores[]": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
-	},
-	{
 	  name: "Ben Affleck",
 	  photo: "https://m.media-amazon.com/images/M/MV5BMTI4MzIxMTk0Nl5BMl5BanBnXkFtZTcwOTU5NjA0Mg@@._V1_UX140_CR0,0,140,209_AL_.jpg",
 	  "scores[]": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
-	},
-	{
-	  name: "George Clooney",
-	  photo: "https://m.media-amazon.com/images/M/MV5BMjEyMTEyOTQ0MV5BMl5BanBnXkFtZTcwNzU3NTMzNw@@._V1_UY209_CR7,0,140,209_AL_.jpg",
-	  "scores[]": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
-	},
-	{
-	  name: "Liam Hemsworth",
-	  photo: "https://m.media-amazon.com/images/M/MV5BMTQ5ODI0MDc4M15BMl5BanBnXkFtZTgwNTM5MDk3MTE@._V1_UX140_CR0,0,140,209_AL_.jpg",
-	  scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
 	}
 ];
   
